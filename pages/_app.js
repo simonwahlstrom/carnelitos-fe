57d6d285232eb41bb65a5085d5e2c0798bbb2a43
Simon Wahlstrom <simon.wahlstrom@sappa.se>
2022-04-21 20:37:41 +0200

    Antd css.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import '../styles/home.scss'
+import "../styles/home.scss"
 import "../styles/login.scss"
 import "../styles/sessions.scss"
 import "../styles/settings.scss"
 import "../styles/tabs.scss"
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
