02797309d0ae607ecafe736cdbf269129e344238
Simon Wahlstrom <simon.wahlstrom@sappa.se>
2022-04-23 14:52:30 +0200

    Test PWA.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: false,
   env: {
     CARNE_API_URL: process.env.NODE_ENV == "production" ? "https://my-app-qht4e.ondigitalocean.app/" : "http://localhost:3000",
-  }
-}
-
-module.exports = nextConfig
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
