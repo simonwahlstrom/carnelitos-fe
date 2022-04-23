7b6759532d3592e1486a9b480ab8e6d929726df7
Simon Wahlstrom <simon.wahlstrom@sappa.se>
2022-04-23 20:18:30 +0200

    Fix notch issue?

--- src/components/shared/layout.js
+++ src/components/shared/layout.js
@@ -73,7 +73,8 @@ export default function Layout(props) {
         <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#9ac05a" />
         <meta name="theme-color" content="#ffffff" />
-        {/* <meta name="apple-mobile-web-app-capable" content="yes"></meta> */}
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <title>Carnelitos</title>
       </Head>
       <>
